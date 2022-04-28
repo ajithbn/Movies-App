c95bfaa1d8528a29cbb200cd15dd77957e69448a
ajithbn <ajithbn4@gmail.com>
2022-04-28 20:31:08 +0530

    text changed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MoviesContainer from './components/MoviesContainer';
 function App() {
   return (
     <div className='container'>
-        <h2 className='row shadow-sm p-2' style={{backgroundColor:'#000000', color: '#fff',textTransform:'uppercase', textShadow: '0px 6px 13px #fff' }}>My Big Movie List</h2>
+        <h2 className='row shadow-sm p-2' style={{backgroundColor:'#000000', color: '#fff',textTransform:'uppercase', textShadow: '0px 6px 13px #fff' }}>My Big Movies List</h2>
         <MoviesContainer />
 
     </div>
